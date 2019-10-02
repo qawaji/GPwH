e3f5de0a79be248ae43f5b6bb084833a344661b2
川路慎一 <bowmoq@gmail.com>
2019-10-03 06:26:48 +0900

    ex 5.2

--- lesson05/exercise.hs
+++ lesson05/exercise.hs
@@ -6,3 +6,6 @@ ifEven f n = if even n
 ifEvenInc = ifEven (\x -> x + 1)
 ifEvenDouble = ifEven (\x -> x * 2)
 ifEvenSquare = ifEven (\x -> x ^ 2)
+
+-- 5.2
+binaryPartialApplication f x1 = (\x2 -> f x1 x2)
