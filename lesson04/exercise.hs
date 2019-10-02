983c1d34da1e35c402faeaccfc2924be7373fbc1
川路慎一 <bowmoq@gmail.com>
2019-10-02 17:05:51 +0900

    lesson 4.2

--- lesson04/exercise.hs
+++ lesson04/exercise.hs
@@ -1,5 +1,6 @@
 import Data.List
 
+-- 4.1
 names = [("Ian", "Curtis"),
          ("Bernard", "Summer"),
          ("Peter", "Hook"),
@@ -8,3 +9,31 @@ names = [("Ian", "Curtis"),
 compareLastNames name1 name2 = lastName1 `compare` lastName2
   where lastName1 = snd name1
         lastName2 = snd name2
+
+-- 4.2
+sfOffice name = if lastName < "L"
+                then nameText
+                     ++ " - PO Box 1234 - San Francisco, CA, 94111"
+                else nameText
+                     ++ " - PO Box 1010 - San Francisco, CA, 94109"
+  where lastName = snd name
+        nameText = (fst name) ++ " " ++ lastName
+
+nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
+  where nameText = (fst name) ++ " " ++ (snd name)
+
+renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"
+  where nameText = snd name
+
+dcOffice name = nameText ++ " PO Box 1337 - Washington DC, 20001"
+  where nameText = (fst name) ++ " " ++ (snd name) ++ ", Esq."
+
+getLocationFunction location = case location of
+  "ny" -> nyOffice
+  "sf" -> sfOffice
+  "reno" -> renoOffice
+  "dc" -> dcOffice
+  _ -> (\name -> (fst name) ++ " " ++ (snd name))
+
+addressLetter name location = locationFunction name
+  where locationFunction = getLocationFunction location
