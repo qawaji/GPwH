74f7f921886063281f910c828929d62fc3f96699
川路慎一 <bowmoq@gmail.com>
2019-10-12 20:31:14 +0900

    add lesson 15 rotn.hs

--- lesson15/rotn.hs
+++ lesson15/rotn.hs
@@ -20,3 +20,38 @@ fourLetterEncorder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
 fourLetterEncorder vals = map rot4l vals
   where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
         rot4l = rotN alphaSize
+
+data ThreeLetterAlphabet = Alpha
+                         | Beta
+                         | Kappa deriving (Show, Enum, Bounded)
+            
+threeLetterMessage :: [ThreeLetterAlphabet]
+threeLetterMessage = [Alpha,Alpha,Beta,Alpha,Kappa]
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder vals = map rot3l vals
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3l = rotN alphaSize
+
+rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNdecoder n c = toEnum rotation
+  where halfN = n `div` 2
+        offset = if even n
+                 then fromEnum c + halfN
+                 else 1 + fromEnum c + halfN
+        rotation = offset `mod` n
+
+threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterDecoder vals = map rot3ldecoder vals
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3ldecoder = rotNdecoder alphaSize
+
+rotEncoder :: String -> String
+rotEncoder text = map rotChar text
+  where alphaSize = 1 + fromEnum (maxBound :: Char)
+        rotChar = rotN alphaSize
+
+rotDecoder :: String -> String
+rotDecoder text = map rotCharDecoder text
+  where alphaSize = 1 + fromEnum (maxBound :: Char)
+        rotCharDecoder = rotNdecoder alphaSize
